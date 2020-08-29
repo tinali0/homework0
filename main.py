be98eb6b14dd1ea1fb4f66daee398430e7aba4f0
tinali0 <xzl5423@psu.edu>
2020-08-29 01:51:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+temperature=input("Enter temperature in celsius: " )
+temperature=float(temperature)
+fahrenheit=float(temperature*9/5+32)
+print(str(temperature) + "°" +" in Celsius is equivalent to "+ str(fahrenheit)+"°"+ " Fahrenheit.")
